da23dcb7e9a8bf0dde20a8906ca1dca60ce4f968
bmurdock@gmail.com <bmurdock@gmail.com>
2020-11-13 18:25:34 -0700

    fix for install when .config/i3* doesn't already exist

--- install.sh
+++ install.sh
@@ -3,8 +3,12 @@
 set -e
 set -x
 REPO_DIR=$(pwd)
-mv -f ~/.config/i3{,bak}
-mv -f ~/.config/i3status{,bak}
+if [ -d ~/.config/i3 ]; then
+    mv -f ~/.config/i3{,bak}
+fi
+if [ -d ~/.config/i3status ]; then
+    mv -f ~/.config/i3status{,bak}
+fi
 mkdir -p ~/.config/i3/
 mkdir -p ~/.config/i3status/
 cd ~/.config/i3/
